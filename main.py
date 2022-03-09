d6e2c0e00eb132e6a9f76276a2d1f52286f24fe8
Farai-Mashiri <f.mashiri.mashiri@gmail.com>
2022-03-09 10:40:46 +0200

    updated code commit

--- main.py
+++ main.py
@@ -3,6 +3,29 @@
 #  This application is used to calculate the quotations of orders placed
 
 #  define the functions used by the application
+def main_menu(user_prog_option):
+    print('This is the main menu')
+    user_prog_option = int(user_prog_option)
+    # Program option Quotation with prices
+    if user_prog_option == 1:
+        print(f'You have chosen option number {user_prog_option}: Quotation with prices')
+    # Program option Quotation without prices
+    elif user_prog_option == 2:
+        print(f'You have chosen option number {user_prog_option}: Quotation without prices')
+    #  Program option Product Information
+    elif user_prog_option == 3:
+        print(f'You have chosen option number {user_prog_option}: Product Information')
+    #  Program option Order load information
+    elif user_prog_option == 4:
+        print(f'You have chosen option number {user_prog_option}: Order load information')
+    #  Program option EXit
+    elif user_prog_option == 5:
+        exit_option()
+    # Program invalid option
+    else:
+        print('You have selected an invalid option, Please insert a valid option and try again')
+
+
 def prod_info(user_prod_name):
     print(f'You have chosen to view information on the {user_prod_name} product')
 
@@ -19,6 +42,19 @@ def load_order():
     print('You have chosen to view order load information')
     print('Below you will find the information for the load order requested')
 
+
+def exit_option():
+    print('Confirm you want to exit? \n 1: - Exit \n 2: Cancel')
+    # requesting a users input and converting it into an integer using the int() functio
+    confirm_exit = int(input('Option here: '))
+    if confirm_exit == 1:
+        print('Goodbye, Thank you for using this Bricks R Us application')
+        return exit(0)
+
+    else:
+        print('Welcome back to the application')
+
+
 #  Print introduction to application
 print('Welcome to the BRU App')
 print('Please select the programme you wish to run')
@@ -27,23 +63,5 @@ user_prog_option = input('1: Quotation with prices\n'
                          '3: Product Information\n'
                          '4: Order load information\n')
 
-user_prog_option = int(user_prog_option)
-# Program option Quotation with prices
-if user_prog_option == 1:
-    print(f'You have chosen option number {user_prog_option}: Quotation with prices')
-# Program option Quotation without prices
-elif user_prog_option == 2:
-    print(f'You have chosen option number {user_prog_option}: Quotation without prices')
-#  Program option Product Information
-elif user_prog_option == 3:
-    print(f'You have chosen option number {user_prog_option}: Product Information')
-# Program option Order load information
-elif user_prog_option == 4:
-    print(f'You have chosen option number {user_prog_option}: Order load information')
-# Program invalid option
-else:
-    print('You have selected an invalid option, Please insert a valid option and try again')
-
-
 user_prod_name = input('Please enter the product you would like to view below: \n')
-
+main_menu(user_prog_option)
